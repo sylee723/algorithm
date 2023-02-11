4018e8d3b84cce4b7106229d8556a90af11e302d
sylee723 <seoyeonlee0723@gmail.com>
2023-02-11 20:38:59 +0900

    0211

--- src/boj/s2/Boj7507_올림픽_게임.java
+++ src/boj/s2/Boj7507_올림픽_게임.java
@@ -3,12 +3,8 @@ package boj.s2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Boj7507_올림픽_게임 {
@@ -17,40 +13,45 @@ public class Boj7507_올림픽_게임 {
 		int T = Integer.parseInt(br.readLine());
 		for (int t = 1; t <= T; t++) {
 			int M = Integer.parseInt(br.readLine());
-			int[][] game = new int[M][3];
+			Game[] games = new Game[M];
 			int days = 0;
 			for (int m = 0; m < M; m++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
-				game[m][0] = Integer.parseInt(st.nextToken());
-				game[m][1] = Integer.parseInt(st.nextToken());
-				game[m][2] = Integer.parseInt(st.nextToken());
-
-				days = Math.max(days, game[m][0]); // 경기가 며칠까지 있는지
+				games[m] = new Game(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()),
+						Integer.parseInt(st.nextToken()));
+				days = Math.max(days, games[m].day); // 경기가 며칠까지 있는지
 			}
-			Arrays.sort(game, new Comparator<int[]>() {
+
+			Arrays.sort(games, new Comparator<Game>() { // 경기를 날짜순, 종료 시간순으로 정렬
 				@Override
-				public int compare(int[] o1, int[] o2) {
-					if (o1[0] == o2[0])
-						return o1[2] - o2[2];
-					return o1[0] - o2[0];
+				public int compare(Game o1, Game o2) {
+					if (o1.day == o2.day)
+						return o1.end - o2.end;
+					return o1.day - o2.day;
 				}
-
 			});
+
+			int count = 0; // 볼 수 있는 총 경기 수
+			int[] end_time = new int[days + 1]; // 날짜별 경기 종료 시간 저장
 			for (int i = 0; i < M; i++) {
-				System.out.println(Arrays.toString(game[i]));
+				int day = games[i].day;
+				if (games[i].start >= end_time[day]) { // 시간이 겹치지 않으면
+					end_time[day] = games[i].end;
+					count++;
+				}
 			}
-			int i =0;
-			int day = game[0][0];
-			int[] count = new int[days + 1];
-			while(true) {
-				if (day != game[i][0])
-					day = game[i][0];
-				
-				
-				count[day]++;
-				
-			}
-//			System.out.println("Scenario #" + t + ":");
+
+			System.out.println("Scenario #" + t + ":\n" + count + "\n");
+		}
+	}
+
+	static class Game {
+		int day, start, end;
+
+		public Game(int day, int start, int end) {
+			this.day = day;
+			this.start = start;
+			this.end = end;
 		}
 	}
 }
