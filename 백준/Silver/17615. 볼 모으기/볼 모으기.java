948b7a37387b5c9597e6cf312eb4d282e5d814c5
이서연 <101053037+sylee723@users.noreply.github.com>
2024-09-23 16:45:43 +0900

    [Silver I] Title: 볼 모으기, Time: 216 ms, Memory: 19920 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17615. 볼 모으기/볼 모으기.java
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.java
@@ -8,12 +8,12 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		char[] ball = br.readLine().toCharArray();
 
+		// 오른쪽으로 이동
 		int redCnt = 0;
 		int blueCnt = 0;
-
 		boolean moveRed = false;
 		boolean moveBlue = false;
-		for (int i = ball.length - 1; i >= 0; i--) {
+		for (int i = N - 1; i >= 0; i--) {
 			if (!moveRed && ball[i] == 'B') {
 				moveRed = true;
 			}
@@ -28,8 +28,31 @@ public class Main {
 				blueCnt++;
 			}
 		}
-
 		int answer = Math.min(redCnt, blueCnt);
+
+		// 왼쪽으로 이동
+		redCnt = 0;
+		blueCnt = 0;
+		moveRed = false;
+		moveBlue = false;
+		for (int i = 0; i < N; i++) {
+			if (!moveRed && ball[i] == 'B') {
+				moveRed = true;
+			}
+			if (!moveBlue && ball[i] == 'R') {
+				moveBlue = true;
+			}
+
+			if (moveRed && ball[i] == 'R') {
+				redCnt++;
+			}
+			if (moveBlue && ball[i] == 'B') {
+				blueCnt++;
+			}
+		}
+		answer = Math.min(answer, redCnt);
+		answer = Math.min(answer, blueCnt);
+
 		System.out.println(answer);
 	}
 }
